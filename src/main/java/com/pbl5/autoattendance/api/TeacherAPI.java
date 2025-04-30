37aba47786cb9637a53adf50cda049b1114ef195
Tai <bintolo1b@gmail.com>
2025-04-30 16:06:50 +0700

    add upate student, teacher, avatar api

--- src/main/java/com/pbl5/autoattendance/api/TeacherAPI.java
+++ src/main/java/com/pbl5/autoattendance/api/TeacherAPI.java
@@ -3,13 +3,15 @@ package com.pbl5.autoattendance.api;
 import com.pbl5.autoattendance.dto.TeacherDTO;
 import com.pbl5.autoattendance.model.Teacher;
 import com.pbl5.autoattendance.service.TeacherService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/teacher")
@@ -24,11 +26,25 @@ public class TeacherAPI {
     @GetMapping
     @PreAuthorize("hasRole('TEACHER')")
     public ResponseEntity<TeacherDTO> getCurrentTeacher(){
-
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         TeacherDTO dto = convertToTeacherDTO(teacherService.getTeacherByUsername(username));
-        System.out.println("bro");
         return new ResponseEntity<>(dto, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('TEACHER')")
+    @PutMapping
+    public ResponseEntity<?> updateTeacher(@Valid @RequestBody TeacherDTO teacherDTO){
+        Map<String, String> map = new HashMap<>();
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Teacher teacher = teacherService.findTeacherByEmail(teacherDTO.getEmail());
+        if (teacher != null && !teacher.getUser().getUsername().equals(username)){
+            map.put("message", "Email is already exist");
+            return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
+        }
+
+        teacherService.updateTeacher(teacherDTO, username);
+        map.put("message", "Update successfully");
+        return new ResponseEntity<>(map, HttpStatus.OK);
     }
 
     private TeacherDTO convertToTeacherDTO(Teacher teacher){
--- src/main/java/com/pbl5/autoattendance/dto/TeacherDTO.java
+++ src/main/java/com/pbl5/autoattendance/dto/TeacherDTO.java
@@ -1,5 +1,9 @@
 package com.pbl5.autoattendance.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,8 +13,14 @@ import lombok.Setter;
 @Setter
 public class TeacherDTO {
     private Integer id;
+    @NotNull(message = "Tên không được để trống")
+    @NotBlank(message = "Tên không được để trống")
     private String name;
+
+    @Pattern(regexp = "^[0-9]{10,11}$", message = "Số điện thoại không hợp lệ")
     private String phone;
+
+    @Email(message = "Email không hợp lệ")
     private String email;
     private String username;
 }
